21d63974a37616b5142fe6f1b5a71f2cfe34d242
Ifeanyi Okoloma <ifeanyiokoloma@gmail.com>
2023-05-04 00:33:13 +0100

    Changed linkedin url

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,7 +1,7 @@
 export const socialsData = {
   github: "https://github.com/ifeanyiOkoloma",
   facebook: "https://facebook.com/ifeanyiOkoloma",
-  linkedIn: "https://www.linkedin.com/ng/ifeanyiokoloma",
+  linkedIn: "https://www.linkedin.com/in/ifeanyi-okoloma-5b5703202",
   instagram: "",
   codepen: "",
   twitter: "https://twitter.com/ifeanyiOkoloma",
